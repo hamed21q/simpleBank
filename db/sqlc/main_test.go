88325662010ac6d74c1fe5692bc2be3c98ef61b6
hamed21q <hasanihamed220@gmail.com>
2024-07-15 12:12:01 -0700

    default postgres port in source code

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:1qaz@localhost:5420/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:1qaz@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
